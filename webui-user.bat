c36f0a57eb2464eaa790540584a968bed4b47ebf
Risharde Ramnath <risharde@gmail.com>
2023-05-08 01:15:14 -0400

    5600G command line arguments

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--listen --lowvram --precision full --no-half --opt-sub-quad-attention --opt-split-attention-v1 --disable-nan-check
 
 call webui.bat
